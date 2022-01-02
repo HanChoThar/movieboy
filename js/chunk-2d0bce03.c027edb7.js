32545b2b24e0c3ebe1f1012d9b1aa0ab38d116fe
hanchothar <henrycho626@gmail.com>
2022-01-02 22:08:50 +0630

    firstStage

---
+++ js/chunk-2d0bce03.c027edb7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bce03"],{"2a31":function(t,e,n){"use strict";n.r(e);var c=n("7a23"),o={class:"text-center mt-16"},s=Object(c["h"])("h2",{class:"text-4xl font-bold mb-4 text-gray-700 text-center"},"Oops! something went wrong",-1),r=Object(c["h"])("h3",{class:"text-gray-700 text-2xl font-bold"},"404 NOT FOUND",-1),a=[s,r];function b(t,e){return Object(c["u"])(),Object(c["g"])("div",o,a)}var d=n("6b0d"),l=n.n(d);const u={},w=l()(u,[["render",b]]);e["default"]=w}}]);
+//# sourceMappingURL=chunk-2d0bce03.c027edb7.js.map
